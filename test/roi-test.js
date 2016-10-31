b80ebb0827ff6e4c1c7e6219d43a6fda443ea389
helio-frota <00hf11@gmail.com>
2016-10-31 13:02:53 -0300

    Build fix eslint.

--- test/roi-test.js
+++ test/roi-test.js
@@ -65,32 +65,32 @@ function createRedirectServer () {
     if (url.parse(request.url).pathname === '/01.html') {
       if (request.method === 'POST') {
         response.writeHead(301, {'content-type': 'text/html',
-        'Location': 'http://localhost:3000/posts'});
+          'Location': 'http://localhost:3000/posts'});
         response.end('Redirected to another server.');
       } else if (request.method === 'PUT') {
         response.writeHead(301, {'content-type': 'text/html',
-        'Location': 'http://localhost:3000/posts/1'});
+          'Location': 'http://localhost:3000/posts/1'});
         response.end('Redirected to another server.');
       } else if (request.method === 'DELETE') {
         response.writeHead(301, {'content-type': 'text/html',
-        'Location': 'http://localhost:3000/posts/3'});
+          'Location': 'http://localhost:3000/posts/3'});
         response.end('Redirected to another server.');
       } else {
         response.writeHead(301, {'content-type': 'text/html',
-        'Location': 'http://localhost:3001/02.html'});
+          'Location': 'http://localhost:3001/02.html'});
         response.end('Redirected from 01.');
       }
     } else if (url.parse(request.url).pathname === '/02.html') {
       response.writeHead(301, {'content-type': 'text/html',
-      'Location': 'http://localhost:3001/01.html'});
+        'Location': 'http://localhost:3001/01.html'});
       response.end('Redirected from 02.');
     } else if (url.parse(request.url).pathname === '/05.html') {
       response.writeHead(301, {'content-type': 'text/html',
-      'Location': 'http://localhost:3000/posts'});
+        'Location': 'http://localhost:3000/posts'});
       response.end('Redirected from 05.');
     } else if (url.parse(request.url).pathname === '/06.html') {
       response.writeHead(301, {'content-type': 'text/html',
-      'Location': 'http://localhost:3000/postaaaa'});
+        'Location': 'http://localhost:3000/postaaaa'});
       response.end('Redirected from 06.');
     }
   });
